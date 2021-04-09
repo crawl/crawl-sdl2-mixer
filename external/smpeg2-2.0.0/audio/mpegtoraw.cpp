8eeb36fc25a3fc66c198d86f8d3291b100b76353
robertxgray <robertxgray@gmail.com>
2021-04-10 00:00:29 +0200

    Build with latest Android NDK

--- external/smpeg2-2.0.0/audio/mpegtoraw.cpp
+++ external/smpeg2-2.0.0/audio/mpegtoraw.cpp
@@ -448,7 +448,9 @@ int Play_MPEGaudio(MPEGaudio *audio, Uint8 *stream, int len)
 	if (audio->timestamp[0] != -1){
 	    double timeshift = audio->Time() - audio->timestamp[0];
 	    double correction = 0;
+#ifndef __ANDROID__
 	    assert(audio->timestamp >= 0);
+#endif
 	    if (fabs(timeshift) > 1.0){
 	        correction = -timeshift;
 #ifdef DEBUG_TIMESTAMP_SYNC
